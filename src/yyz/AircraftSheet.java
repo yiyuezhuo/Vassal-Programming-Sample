4b4db162b8b0e05e4f0406c56ba6fb58d2aa75bb
yiyuezhuo <yiyuezhuo@gmail.com>
2024-12-21 10:49:19 +0800

    Fire and waypoint plotting

--- src/yyz/AircraftSheet.java
+++ src/yyz/AircraftSheet.java
@@ -4,20 +4,23 @@ import VASSAL.build.GameModule;
 import VASSAL.build.module.Chatter;
 import VASSAL.build.module.documentation.HelpFile;
 import VASSAL.command.ChangePiece;
+import VASSAL.command.ChangeTracker;
 import VASSAL.command.Command;
+import VASSAL.command.RemovePiece;
 import VASSAL.configure.IntConfigurer;
 import VASSAL.counters.Decorator;
 import VASSAL.counters.GamePiece;
 import VASSAL.counters.KeyCommand;
+import VASSAL.counters.PieceFinder;
 import VASSAL.tools.SequenceEncoder;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
 
-public class AircraftSheet extends Decorator {
+public class AircraftSheet extends Decorator implements MouseListener {
     // ID is used as the serialization identification head.
     // ";" is introduced to remove ambiguity in a "startswith" based custom Command Encoder, you can use other format if you really know what you are doing.
     public static final String ID = "AircraftSheet;";
@@ -39,6 +42,13 @@ public class AircraftSheet extends Decorator {
         var sd = new SequenceEncoder.Decoder(s, ';');
 
         mediumDetectionRange = sd.nextInt(0);
+        var numWaypoints = sd.nextInt(0);
+        waypoints.clear();
+        for(var i=0; i<numWaypoints; i++){
+            var x = sd.nextInt(0);
+            var y = sd.nextInt(0);
+            waypoints.add(new Point(x, y));
+        }
     }
 
     @Override
@@ -46,6 +56,13 @@ public class AircraftSheet extends Decorator {
         var se = new SequenceEncoder(';');
 
         se.append(mediumDetectionRange);
+
+        se.append(waypoints.size());
+        for (Point waypoint : waypoints) {
+            se.append(waypoint.x);
+            se.append(waypoint.y);
+        }
+
         return se.getValue();
     }
 
@@ -64,12 +81,18 @@ public class AircraftSheet extends Decorator {
 
     private KeyCommand[] commands;
     private final KeyStroke openCommand = KeyStroke.getKeyStroke(KeyEvent.VK_A, 0);
+    private final KeyStroke fireCommand = KeyStroke.getKeyStroke(KeyEvent.VK_F, 0);
+    private final KeyStroke waypointCommand = KeyStroke.getKeyStroke(KeyEvent.VK_F3, 0);
+    private final KeyStroke concludeMouseCommand = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
 
     @Override
     protected KeyCommand[] myGetKeyCommands() {
         if(commands == null){
             commands = new KeyCommand[]{
-                new KeyCommand("Open Aircraft Sheet", openCommand, this)
+                    new KeyCommand("Open Aircraft Sheet", openCommand, this),
+                    new KeyCommand("Fire", fireCommand, this),
+                    new KeyCommand("Plot Waypoint", waypointCommand, this),
+                    new KeyCommand("Conclude Plot Waypoint", concludeMouseCommand, this)
             };
         }
         return commands;
@@ -127,12 +150,51 @@ public class AircraftSheet extends Decorator {
         frame.setVisible(true);
     }
 
+    enum MouseMode{
+        FIRING,
+        WAYPOINTPLOTTING
+    }
+
+    MouseMode mouseMode;
+
     @Override
     public Command myKeyEvent(KeyStroke keyStroke) {
         myGetKeyCommands();
         if(keyStroke.equals(openCommand)){
             // JOptionPane.showMessageDialog(null, "Hello custom code for Vassal");
             handleOpen();
+        }else if(keyStroke.equals(fireCommand)){
+            if(mouseMode == null){ // Permit only one "queued" firing target selection.
+                mouseMode = MouseMode.FIRING;
+                getMap().pushMouseListener(this); // replace default handler with AircraftSheet
+            }
+        }else if(keyStroke.equals(waypointCommand)){
+            if(mouseMode == null){
+                mouseMode = MouseMode.WAYPOINTPLOTTING;
+                getMap().pushMouseListener(this);
+
+                // waypoints.clear();
+                tempWaypoints.clear();
+            }
+        }else if(keyStroke.equals(concludeMouseCommand)){
+            getMap().popMouseListener();
+            if(mouseMode == MouseMode.FIRING){ // cancel firing
+                mouseMode = null;
+            }else if(mouseMode == MouseMode.WAYPOINTPLOTTING){ // commit temp waypoints
+                var changeTracker = new ChangeTracker(getOutermost(this));
+
+                waypoints.clear();
+                waypoints.addAll(tempWaypoints);
+                tempWaypoints.clear();
+                mouseMode = null;
+
+                var changeCommand = changeTracker.getChangeCommand();
+                var mod = GameModule.getGameModule();
+                var c = new Chatter.DisplayText(mod.getChatter(), "Set Waypoint");
+                c.execute();
+                c.append(changeCommand);
+                mod.sendAndLog(c);
+            }
         }
 
         return null;
@@ -163,6 +225,44 @@ public class AircraftSheet extends Decorator {
             g2d.drawOval(xs, ys, r, r);
         }
 
+        if(isSelected()){
+            if(!waypoints.isEmpty()){
+                drawWaypoints(g, waypoints, new Color(0,0,0));
+            }
+            if(!tempWaypoints.isEmpty()){
+                drawWaypoints(g, tempWaypoints, new Color(100, 100, 100));
+            }
+        }
+    }
+
+    void drawWaypoints(Graphics g, ArrayList<Point> waypoints, Color color){
+        // TODO: Move to static or utilities
+        // https://stackoverflow.com/questions/9771924/animating-dashed-line-with-java-awt-basicstroke
+        float dash[] = {5.0f,5.0f};
+        BasicStroke dashedStroke = new BasicStroke(
+                3f,
+                BasicStroke.CAP_ROUND,
+                BasicStroke.JOIN_MITER,
+                1.5f, //miter limit
+                dash,
+                0
+        );
+        final Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(dashedStroke);
+
+        g2d.setColor(color);
+
+        final double os_scale = g2d.getDeviceConfiguration().getDefaultTransform().getScaleX();
+        var map = piece.getMap();
+
+        var p0 = piece.getPosition();
+        var pp = p0;
+        for(var p: waypoints) {
+            var ppd = map.mapToDrawing(pp, os_scale);
+            var pd = map.mapToDrawing(p, os_scale);
+            g.drawLine(ppd.x, ppd.y, pd.x, pd.y);
+            pp = p;
+        }
     }
 
     @Override
@@ -178,5 +278,76 @@ public class AircraftSheet extends Decorator {
     @Override
     public String getName() {
         return piece.getName(); // delegate to the inner piece
+    }
+
+    static Random rand = new Random();
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+//        switch(mouseMode){
+//            case FIRING:
+//                handleFire(e);
+//                break;
+//            case WAYPOINTPLOTTING:
+//                handleWaypointPlotting(e);
+//                break;
+//        }
+    }
+
+    void handleFire(MouseEvent e){
+        getMap().popMouseListener(this); // restore default behaviour
+        mouseMode = null;
+
+        var mapPos = e.getPoint(); // Position in the map coordinate
+        // JOptionPane.showMessageDialog(null, mapPos);
+        var piece = getMap().findPiece(e.getPoint(), PieceFinder.PIECE_IN_STACK);
+        if(piece != null){
+            var roll = rand.nextDouble();
+            var hit = roll <= 0.5;
+
+            var mod = GameModule.getGameModule();
+            var c = new Chatter.DisplayText(mod.getChatter(), String.format("Firing Resolution: %f => %b", roll, hit));
+
+            if(hit){
+                c.append(new RemovePiece(piece));
+            }
+            c.execute();
+            mod.sendAndLog(c);
+        }
+    }
+
+    ArrayList<Point> waypoints = new ArrayList<Point>();
+    ArrayList<Point> tempWaypoints = new ArrayList<Point>();
+
+    void handleWaypointPlotting(MouseEvent e){
+        tempWaypoints.add(e.getPoint());
+        getMap().repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        switch(mouseMode){
+            case FIRING:
+                handleFire(e);
+                break;
+            case WAYPOINTPLOTTING:
+                handleWaypointPlotting(e);
+                break;
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
